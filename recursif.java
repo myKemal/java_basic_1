ab98b73b89fb3a0796313a88a96fd9593eefec61
ALİ KEMAL DURKAYA <adurkaya@bandirma.edu.tr>
2023-12-18 16:15:45 +0300

    blabla

--- recursif.java
+++ recursif.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
+
 public class recursif {
     static Scanner scanner = new Scanner(System.in);
     static String ilksayi;
     static String ikincisayi;
+    static String islem;
+
     public static void main(String[] args) {
         sayiAl1();
-        sayiAl2();
         islem();
+        sayiAl2();
+        hesap();
         scanner.close();
     }
+
     public static void islem() {
         System.out.println("Toplama için + 'e basınız...");
         System.out.println("Çarpma için * 'ye basınız...");
-        String islem = scanner.nextLine();
+        islem = scanner.nextLine();
         System.out.println("Seçiminiz " + islem);
 
+        if (!(islem.equals("+")) && !(islem.equals("*"))) {
+            islem();
+        }
+
+    }
+
+    public static void hesap() {
         if (islem.equals("+")) {
             int sonuc = Integer.parseInt(ilksayi) + Integer.parseInt(ikincisayi);
             System.out.println(ilksayi + " + " + ikincisayi + " = " + sonuc);
@@ -24,13 +36,7 @@ public class recursif {
             System.out.println(ilksayi + " * " + ikincisayi + " = " + sonuc);
 
         }
-        else{
-            System.out.println("Geçersiz işlem...");
-            islem();
-        }
     }
-
-
 
     public static void sayiAl1() {
         System.out.println("İlk sayıyı giriniz:");
@@ -41,6 +47,7 @@ public class recursif {
             sayiAl1();
         }
     }
+
     public static void sayiAl2() {
         System.out.println("İkinci sayıyı giriniz:");
         ikincisayi = scanner.nextLine();
@@ -50,6 +57,7 @@ public class recursif {
             sayiAl2();
         }
     }
+
     public static boolean isInteger(String str, int index) {
         if (index == str.length()) {
             return true;
